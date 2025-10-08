bba0bb1c704b67cfcefcba41d29ae9b87ada8e22
chakradhar_reddy <chakradharreddybheemireddy@gmail.com>
2025-10-08 18:17:53 +0530

    changes dn

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://chakradharreddyb2_db_user:S1m4ReGA748gmK1p@cluster0.os9ptyd.mongodb.net/');
     console.log("MongoDB connected");
   } catch (error) {
     console.error(error);
